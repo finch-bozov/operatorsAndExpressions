25775e56b680fd752773dad8c8263e00c6a40545
Peter Bozov <peter.bozov@gmail.com>
2019-06-11 09:06:11 +0300

    Gosho fixva problemite

--- primeNumberCheck/primeNumberCheck.cs
+++ primeNumberCheck/primeNumberCheck.cs
@@ -14,10 +14,10 @@ namespace primeNumberCheck
             int byThree = parsedNumber % 3;
             int byFive  = parsedNumber % 5;
             int bySeven = parsedNumber % 7;
-            bool isPrime = (parsedNumber <= 100) && (byTwo > 0) && (byThree > 0) && (byFive > 0) && (bySeven > 0);
+            bool isPrime = ((parsedNumber == 2) || (parsedNumber == 3) || (parsedNumber == 5) || (parsedNumber == 7)) || ((byTwo > 0) && (byThree > 0) && (byFive > 0) && (bySeven > 0));
+            bool isPrimeVlado = (parsedNumber % 2 != 0 && parsedNumber != 1 && parsedNumber > 0 && parsedNumber < 101);
             Console.WriteLine(isPrime);
-            double gosho = Math.Sqrt(parsedNumber);
-            Console.WriteLine(gosho);
+            //Console.WriteLine(isPrimeVlado);
         }
     }
 }
